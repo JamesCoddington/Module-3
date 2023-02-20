30e57b682dd2c8f8411ee031b03dc23b0cea8a2f
James Coddington <james@coddington.io>
2023-02-20 15:31:22 -0600

    added way to get the positions and place mines

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
+
+public class Board : MonoBehaviour
+{
+    public Vector3Int position;
+
+    private void Start()
+    {
+        Invoke("SetTiles", 2);
+    }
+
+    private void SetTiles()
+    {
+        Tile[] tiles = GetComponentsInChildren<Tile>();
+        foreach (var tile in tiles)
+        {
+            tile.CheckSides(tiles);
+        }
+        SetMines();
+    }
+
+    private void SetMines()
+    {
+        var bombPlaces = new List<int>();
+        Tile[] tiles = GetComponentsInChildren<Tile>();
+        for (int i = 0; i < 60; i++)
+        {
+            var place = Random.Range(0, tiles.Length);
+            while (bombPlaces.Contains(place))
+            {
+                place = Random.Range(0, tiles.Length);
+            }
+            tiles[place].type = Tile.Type.Mine;
+            bombPlaces.Add(place);
+        }
+        print("Bombs Done");
+    }
+}
